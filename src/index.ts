971037cd7328360f77a8d24f3ffec6998ff7bf88
Alisha Robinson <alkgreer@gmail.com>
2024-03-05 19:33:39 +0000

    styling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,45 @@
-import './index.scss';
+import "./index.scss";
+import OpenAI from "openai";
 
-console.log('Hello, world!');
+document
+  .getElementById("apiForm")
+  .addEventListener("submit", async (event: SubmitEvent) => {
+    event.preventDefault();
+
+    const key = ((event.target as HTMLFormElement)[0] as HTMLInputElement)
+      .value;
+    const prompt = ((event.target as HTMLFormElement)[1] as HTMLTextAreaElement)
+      .value;
+
+    document.getElementById("response").innerText = "Fetching Response ...";
+    startCompletions(key, prompt);
+  });
+
+async function startCompletions(key: string, prompt: string) {
+  const openai = new OpenAI({
+    apiKey: key,
+    dangerouslyAllowBrowser: true,
+  });
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    temperature: 0,
+    max_tokens: 1024,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  console.log(response);
+
+  document.getElementById("response").innerText =
+    response.choices[0].message.content;
+}
